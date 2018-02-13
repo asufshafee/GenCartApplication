f83c32d3c341fc86e1ad0635c4f350f0d9192702
GeeksEra <jaani.asif0333@gmail.com>
2018-02-13 12:44:53 +0500

    Initial commit

---
+++ app/src/main/java/com/webmarke8/app/gencart/Activities/SelectUserActivity.java
@@ -0,0 +1,15 @@
+package com.webmarke8.app.gencart.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.webmarke8.app.gencart.R;
+
+public class SelectUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_user);
+    }
+}
